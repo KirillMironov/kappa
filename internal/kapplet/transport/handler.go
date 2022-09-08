a5a458917b15670cfc81880019d4a9ffb188afcb
Kirill Mironov <22131753+KirillMironov@users.noreply.github.com>
2022-09-08 20:10:49 +0300

    Gob tcp client

--- internal/kapplet/transport/handler.go
+++ internal/kapplet/transport/handler.go
@@ -1,42 +1,47 @@
 package transport
 
 import (
-	"encoding/gob"
 	"github.com/KirillMironov/kappa/internal/kappa/domain"
 	"github.com/KirillMironov/kappa/pkg/logger"
-	"net"
+	"github.com/KirillMironov/kappa/pkg/tcp"
+	"time"
 )
 
 type Handler struct {
-	port   string
-	logger logger.Logger
+	port              string
+	reconnectInterval time.Duration
+	reconnectAttempts int
+	logger            logger.Logger
 }
 
 func NewHandler(port string, logger logger.Logger) *Handler {
 	return &Handler{
-		port:   port,
-		logger: logger,
+		port:              port,
+		reconnectInterval: time.Second * 3,
+		reconnectAttempts: 3,
+		logger:            logger,
 	}
 }
 
-func (h Handler) Start() error {
-	conn, err := net.Dial("tcp", ":"+h.port)
+func (h *Handler) Start() error {
+	client, err := tcp.NewClient[[]domain.Pod]("", h.port)
 	if err != nil {
 		return err
 	}
-	defer conn.Close()
+	defer client.Close()
 
-	var decoder = gob.NewDecoder(conn)
+	client.SetReconnectInterval(h.reconnectInterval)
+	client.SetReconnectAttempts(h.reconnectAttempts)
+
+	podsCh, errorCh := client.Receive()
 
 	for {
-		var pods []domain.Pod
-
-		err = decoder.Decode(&pods)
-		if err != nil {
-			h.logger.Errorf("failed to decode data: %v", err)
+		select {
+		case pods := <-podsCh:
+			h.logger.Infof("received pods: %v", pods)
+		case err = <-errorCh:
+			h.logger.Error(err)
 			return err
 		}
-
-		h.logger.Infof("received pods: %v", pods)
 	}
 }
